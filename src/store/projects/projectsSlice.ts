0a571b61495553a32ff584912324eea7cf2e0287
omar <omarhrn@gmail.com>
2025-01-20 13:27:20 -0300

    Primer estilo del card todo, creada la prueba del reducer para los todos

--- src/store/projects/projectsSlice.ts
+++ src/store/projects/projectsSlice.ts
@@ -4,14 +4,17 @@ const DEFAULT_STATE = [
   {
     id: "1",
     name: "Project 1",
+    todos: [],
   },
   {
     id: "2",
     name: "Project 2",
+    todos: [],
   },
   {
     id: "3",
     name: "Project 3",
+    todos: [],
   },
 ]
 
@@ -19,10 +22,22 @@ export type ProjectId = string;
 
 export interface Project {
   name: string;
+  todos: Todo[];
 }
 
 export interface ProjectWithId extends Project {
   id: ProjectId;
+}
+
+export type TodoId = string;
+
+export interface Todo {
+  title: string;
+  projectId: ProjectId;
+}
+
+export interface TodoWithId extends Todo {
+  id: TodoId;
 }
 
 const initialState: ProjectWithId[] = (() => {
@@ -45,9 +60,30 @@ export const ProjectSlice = createSlice({
       const id = action.payload;
       return state.filter((project) => project.id !== id);
     },
+    updateProject: (state, action: PayloadAction<Project>) => {
+
+    },
+    addTodo: (state, action: PayloadAction<Todo>) => {
+      const { projectId } = action.payload;
+      const project = state.find((project) => project.id === projectId);
+
+      if (!project) {
+        throw new Error(`Project with id ${projectId} not found`);
+      }
+
+      project.todos.push({
+        ...action.payload,
+      });
+    },
+    updateTodo: (state, action: PayloadAction<Todo>) => {
+
+    },
+    deleteTodo: (state, action: PayloadAction<Todo>) => {
+
+    },
   }
 });
 
 export default ProjectSlice.reducer;
 
-export const { addNewProject, deleteProjectById } = ProjectSlice.actions;
+export const { addNewProject, deleteProjectById, addTodo } = ProjectSlice.actions;
